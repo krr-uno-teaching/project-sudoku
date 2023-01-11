5643f327cded76b74995f9f4b2e85c7d3cddd9e9
jorgefandinno <jfandinno@unomaha.edu>
2023-01-11 10:09:35 -0600

    clingo 5.6 compatibility

--- autograder.py
+++ autograder.py
@@ -15,7 +15,7 @@ def dispatch_question(args):
     pass
 
 def call_clingo(clingo, input_names, timeout):
-    cmd = [clingo, "--warn=no-atom-undefined", "--warn=no-file-included", "--warn=no-operation-undefined", "--warn=no-variable-unbounded", "--warn=no-global-variable", "--outf=2"] + input_names
+    cmd = [clingo, "--warn=no-atom-undefined", "--warn=no-file-included", "--warn=no-operation-undefined", "--warn=no-global-variable", "--outf=2"] + input_names
     start = time.time()
     output = run(cmd, stdout=PIPE, stderr=PIPE, timeout=timeout, text=True)
     end = time.time()
